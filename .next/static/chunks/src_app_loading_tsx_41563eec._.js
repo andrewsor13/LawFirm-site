4afa97fac14680910d4cb6771180dc2f987c598c
Ciobancă Andrei-Sorin <103632792+andrewsor13@users.noreply.github.com>
2025-11-05 00:21:28 +0200

    Delete .next directory

--- .next/static/chunks/src_app_loading_tsx_41563eec._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_components_Loader_tsx_ada20ffc._.js"
-],
-    source: "dynamic"
-});
